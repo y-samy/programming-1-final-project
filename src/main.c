7ff89ab2b158add640e1272b61eda89cd718be3d
Youssef Samy <office.youssefkassem@gmail.com>
2024-12-21 00:25:52 +0200

    Fixed date input

--- src/main.c
+++ src/main.c
@@ -2,9 +2,31 @@
 #include <auth/login.h>
 #include <management/reservations.h>
 #include <management/rooms.h>
-#include <libui/io.h>
+
 int main()
 {
-    struct tm date;
-    input_date(&date);
+    /* Deserialize */
+    user_t user = new_user_session();
+    room_t *rooms = load_rooms();
+    size_t room_count = get_room_count();
+    reservation_t *reservations;
+    while (1) {
+        if (root_menu() == ROOT_M_CHOICE_EXIT)
+            return 0;
+        if(login_menu(&user) != LOGIN_M_SUCCES)
+            continue;
+        int menu_choice = main_menu();
+        if (menu_choice == MAIN_M_CHOICE_LOGOUT) {
+            logout(&user);
+            continue;
+        }
+        if (menu_choice == 5) {
+            availability_menu(rooms, room_count);
+        }
+    }
+    /* Unload serialized data */
+    end_user_session();
+    /* Deserialize */
+    save_and_unload_rooms();
+    return 0;
 }
