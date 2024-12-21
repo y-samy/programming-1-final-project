4e999ed1335f7f431d691cf115977a63850f278f
Youssef Samy <office.youssefkassem@gmail.com>
2024-12-21 06:02:28 +0200

    test

--- src/main.c
+++ src/main.c
@@ -7,16 +7,12 @@ int main()
 {
     /* Deserialize */
     user_t user = new_user_session();
-    room_t *rooms = load_rooms();
-    size_t room_count = get_room_count();
-    reservation_t *reservations;
-    while (1) {
-       reserve_room();
-
-    }
+    load_rooms();
+    load_reservations();
+        reserve_room();
     /* Unload serialized data */
     end_user_session();
-    /* Deserialize */
+    save_and_unload_reservations();
     save_and_unload_rooms();
     return 0;
 }
