138f5e13154346c0c3b21ef4eab145e2102d3dee
Youssef Samy <office.youssefkassem@gmail.com>
2024-12-27 01:11:51 +0200

    magic numbers cuz why not

--- src/ui/menus/cancel_reservation.c
+++ src/ui/menus/cancel_reservation.c
@@ -11,9 +11,6 @@
     "CANCEL RESERVATION\n"\
     "------------------\n\n"
 
-#define reservation_id 2
-#define room_id 1
-
 int cancel_reservation_menu(HotelSession *session)
 {
     int id;
@@ -35,41 +32,7 @@ int cancel_reservation_menu(HotelSession *session)
             id_type = status;
         }
         while (stage == 2) {
-            if (id_type == reservation_id) {
-                reservation_t *reservation = NULL;
-                status = input(id_buff, "Reservation ID: ", 10, INPUT_INT_POSITIVE, true);
-                if (status == IO_STATUS_UNDO) {
-                    stage--;
-                    break;
-                }
-                if (status == IO_STATUS_ESC)
-                    return MENU_SIGNAL_CANCEL;
-                if (status == IO_STATUS_EXIT)
-                    return MENU_SIGNAL_EXIT;
-                id = atoi(id_buff);
-                if ((reservation = get_reservation_by_id(session, id)) == NULL) {
-                    printf(CLEAR_LN ERROR_HIGHLIGHT "No reservation found with ID " CLR_TEXT_BLACK "%d" CLR_RESET CUR_UP, id);
-                    continue;
-                }
-                if (reservation->checked_in) {
-                    printf(CLEAR_LN ERROR_HIGHLIGHT "Cannot cancel after check-in." CLR_RESET CUR_UP);
-                    continue;
-                }
-                printf(CLEAR_LN TABLE_HIGHLIGHT "Are you sure you want to cancel the reservation? This action is irreversible.\n\n");
-                status = choices("Confirm\nUndo\nMain Menu\nExit\n");
-                if (status == IO_STATUS_ESC || status == 3)
-                    return MENU_SIGNAL_CANCEL;
-                if (status == IO_STATUS_EXIT || status == 4)
-                    return MENU_SIGNAL_EXIT;
-                if (status == IO_STATUS_UNDO || status == 2) {
-                    stage--;
-                    break;
-                }
-                cancel_reservation(session,id);
-                printf(CLEAR_LN "\nReservation %d has successfully been cancelled\n", id);
-                return MENU_SIGNAL_PROCEED;
-            }
-            if (id_type == room_id) {
+            if (id_type == 1) {
                 room_t *room = NULL;
                 status = input(id_buff, "Room ID: ", 10, INPUT_INT_POSITIVE, true);
                 if (status == IO_STATUS_UNDO) {
@@ -90,6 +53,40 @@ int cancel_reservation_menu(HotelSession *session)
                     continue;
                 }
                 if (room->reservation.checked_in) {
+                    printf(CLEAR_LN ERROR_HIGHLIGHT "Cannot cancel after check-in." CLR_RESET CUR_UP);
+                    continue;
+                }
+                printf(CLEAR_LN TABLE_HIGHLIGHT "Are you sure you want to cancel the reservation? This action is irreversible.\n\n");
+                status = choices("Confirm\nUndo\nMain Menu\nExit\n");
+                if (status == IO_STATUS_ESC || status == 3)
+                    return MENU_SIGNAL_CANCEL;
+                if (status == IO_STATUS_EXIT || status == 4)
+                    return MENU_SIGNAL_EXIT;
+                if (status == IO_STATUS_UNDO || status == 2) {
+                    stage--;
+                    break;
+                }
+                cancel_reservation(session,id);
+                printf(CLEAR_LN "\nReservation %d has successfully been cancelled\n", id);
+                return MENU_SIGNAL_PROCEED;
+            }
+            if (id_type == 2) {
+                reservation_t *reservation = NULL;
+                status = input(id_buff, "Reservation ID: ", 10, INPUT_INT_POSITIVE, true);
+                if (status == IO_STATUS_UNDO) {
+                    stage--;
+                    break;
+                }
+                if (status == IO_STATUS_ESC)
+                    return MENU_SIGNAL_CANCEL;
+                if (status == IO_STATUS_EXIT)
+                    return MENU_SIGNAL_EXIT;
+                id = atoi(id_buff);
+                if ((reservation = get_reservation_by_id(session, id)) == NULL) {
+                    printf(CLEAR_LN ERROR_HIGHLIGHT "No reservation found with ID " CLR_TEXT_BLACK "%d" CLR_RESET CUR_UP, id);
+                    continue;
+                }
+                if (reservation->checked_in) {
                     printf(CLEAR_LN ERROR_HIGHLIGHT "Cannot cancel after check-in." CLR_RESET CUR_UP);
                     continue;
                 }
